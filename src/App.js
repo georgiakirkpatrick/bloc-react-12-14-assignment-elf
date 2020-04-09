be03cfd2429b351e553697e2c41f86dda0a80ff3
Georgia Kirkpatrick <georgia.kirkpatrick@gmail.com>
2020-04-08 19:12:36 -0700

    Add CartTotal, move features to App.js
    Separate CartTotal from Cart to its own component,
    move features array from index.js to App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,49 @@ import './App.css';
 import { CustomizerForm } from './CustomizerForm/CustomizerForm';
 import { Cart } from "./Cart/Cart";
 
+const features = {
+  Processor: [
+    {
+      name: '17th Generation Intel Core HB (7 Core with donut spare)',
+      cost: 700
+    },
+    {
+      name: 'Professor X AMD Fire Breather with sidewinder technology',
+      cost: 1200
+    }
+  ],
+  "Operating System": [
+    {
+      name: 'Ubuntu Linux 16.04',
+      cost: 200
+    },
+    {
+      name: 'Bodhi Linux',
+      cost: 300
+    }
+  ],
+  "Video Card": [
+    {
+      name: 'Toyota Corolla 1.5v',
+      cost: 1150.98
+    },
+    {
+      name: 'Mind mild breeze 2000',
+      cost: 1345
+    }
+  ],
+  Display: [
+    {
+      name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+      cost: 1500
+    },
+    {
+      name: '15.3" HGTV (3840 x 2160) Home makeover edition',
+      cost: 1400
+    },
+  ]
+};
+
 // This object will allow us to
 // easily convert numbers into US dollar values
 const USCurrencyFormat = new Intl.NumberFormat('en-US', {
@@ -57,7 +100,7 @@ export default class App extends Component {
         </header>
 
         <main>
-          <CustomizerForm features={this.props.features} USCurrencyFormat={USCurrencyFormat} updateFeature={this.updateFeature}/>
+          <CustomizerForm features={features} USCurrencyFormat={USCurrencyFormat} updateFeature={this.updateFeature}/>
           <Cart USCurrencyFormat={USCurrencyFormat} selected={selected} />
         </main>
       </div>
--- src/Cart/Cart.js
+++ src/Cart/Cart.js
@@ -1,27 +1,14 @@
 import React from "react";
 import { CartItemList } from "../CartItemList/CartItemList";
+import { CartTotal } from '../CartTotal/CartTotal';
 import './Cart.css';
 
 export const Cart = (props) => {
-    const USCurrencyFormat = props.USCurrencyFormat;
-    const selected = props.selected;
-
-    const total = Object.keys(props.selected).reduce(
-        (acc, curr) => acc + props.selected[curr].cost,
-        0
-      );
-
     return (
         <section className="main__summary">
             <h2>Your cart</h2>
-            <CartItemList selected={selected} USCurrencyFormat={props.USCurrencyFormat}/>
-            <div className="summary__total">
-                <div className="summary__total__label">Total</div>
-                <div className="summary__total__value">
-                    { USCurrencyFormat.format(total) }
-                </div>
-            </div>
+            <CartItemList selected={props.selected} USCurrencyFormat={props.USCurrencyFormat} />
+            <CartTotal selected={props.selected} USCurrencyFormat={props.USCurrencyFormat} />
         </section>
-
     )
 }
